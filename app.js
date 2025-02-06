8859930ca5feba074e90b9239c76f5268e57bf79
Hariharan0407 <haridmt@gmail.com>
2025-02-06 22:36:26 +0530

    Completed password validation using Javascript

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const form = document.getElementById('myform');
-form.addEventListener('submit',function(){
-    
+const password = document.getElementById('mypassword')
+const confirmPassword = document.getElementById('confirmpassword')
+const hurryup = document.getElementById('hurryup')
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    //this prevents the page from refreshing every tiem the submit button is pressed
+    let passwordsMatch = false
+    if(password.value == confirmPassword.value){
+        passwordsMatch = true
+        hurryup.innerText = 'Passwords Match'
+    }
+    else{
+        passwordsMatch = false
+        hurryup.innerText = 'Passwords Do not Match'      
+    }
 })
